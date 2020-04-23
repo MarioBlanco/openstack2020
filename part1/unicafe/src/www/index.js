f33a125ebe2ddd523ccb9e7c7b4063a04fe8603c
Mario Blanco <mbalonso2@gmail.com>
2020-04-23 18:56:02 +0200

    unicafe completed

--- part1/unicafe/src/www/index.js
+++ part1/unicafe/src/www/index.js
@@ -23,16 +23,77 @@ const App = () => {
         )
     }
 
+    const Statistic = ({text,value})=>{
+        return(
+            <>
+                {text} {value}
+            </>
+        )
+    }
+
+    const Statistics = ({good,neutral,bad})=>{
+        if(good+neutral+bad>0) {
+            return (
+                <>
+                    <p>statistics</p>
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td><Statistic text="Good" value={good}/></td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <Statistic text="Neutral" value={neutral}/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <Statistic text="Bad" value={bad}/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <Statistic text="All" value={good + neutral + bad}/>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <Statistic text="Average" value={(good * 1 + bad * -1) / (good + neutral + bad)}/>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <Statistic text="positive" value={(good / (good + neutral + bad) * 100)+" %"} />
+
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+
+
+
+
+                </>
+            )
+        }else{
+            return(
+                <p>
+                    No feedback given
+                </p>
+            )
+        }
+
+    }
+
     return (
         <div>
             <p>give feedback</p>
             <ButtonFeedback state={good} setState={setGood} type="Good"/>
             <ButtonFeedback state={neutral} setState={setNeutral} type="Neutral"/>
             <ButtonFeedback state={bad} setState={setBad} type="Bad"/>
-            <p>statistics</p>
-            <p>Good {good}</p>
-            <p>Neutral {neutral}</p>
-            <p>Bad {bad}</p>
+            <Statistics good={good} neutral={neutral} bad={bad}/>
         </div>
     )
 }
