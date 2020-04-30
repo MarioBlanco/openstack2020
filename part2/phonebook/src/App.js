07e8b582f82da704d025ecd0306d5864f8e3b112
Mario Blanco <mbalonso2@gmail.com>
2020-04-30 13:57:12 +0200

    phonebook with effect

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,14 +1,27 @@
-import React, {useState} from 'react'
+import React, {useState,useEffect} from 'react'
 import Persons from "./Components/Persons";
 import PersonForm from "./Components/PersonForm";
 import Filter from "./Components/Filter";
+import axios from "axios";
 const App = () => {
     const [ persons, setPersons ] = useState([
-        { name: 'Arto Hellas', number: '040-1234567' },
+        /*{ name: 'Arto Hellas', number: '040-1234567' },
         { name: 'Ada Lovelace', number: '039-44-5323523' },
         { name: 'Dan Abromov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }*/
     ])
+
+    useEffect(()=>{
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response=>{
+                console.log('promise fulfilled')
+                setPersons(response.data)
+                setFiltered(response.data)
+            })
+        },[])
+
     const [ newName, setNewName ] = useState('')
     const [ newNumber, setNewNumber ] = useState('')
     const [ newFilter, setNewFilter ] = useState('')
