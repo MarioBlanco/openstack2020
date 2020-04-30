021f3eb06394ea71cb685bf08e196e142c321851
Mario Blanco <mbalonso2@gmail.com>
2020-04-30 15:27:21 +0200

    countries2.12 completed

---
+++ part2/datacountries/src/App.js
@@ -0,0 +1,102 @@
+import React, {useState, useEffect} from 'react';
+import Axios from "axios";
+
+
+const App = () => {
+    const [countries, setCountries] = useState([])
+    const [newFilter, setNewFilter] = useState('')
+    const [filtered, setFiltered] = useState([])
+    useEffect(() => {
+        Axios
+            .get('https://restcountries.eu/rest/v2/all')
+            .then(response => {
+                setCountries(response.data)
+                setFiltered(response.data)
+            })
+    }, [])
+
+    useEffect(()=>{
+        setFiltered(countries.filter(c=>c.name.toUpperCase().includes(newFilter.toUpperCase())))
+        },[newFilter])
+    const handleFilterChange = (event) => {
+        setNewFilter(event.target.value)
+    }
+
+    const Country = ({country}) => {
+        return (
+            <li>
+                {country.name}
+            </li>
+        )
+    }
+
+    const ListCountries = ({countries}) => {
+        if (filtered.length === 0) {
+            return (
+                <div>
+                    No match found, sorry!
+                </div>
+            )
+        }
+        if (filtered.length === 1) {
+            let theCountry = {
+                name: filtered[0].name,
+                capital: filtered[0].capital,
+                population: filtered[0].population,
+                languages: filtered[0].languages,
+                flag: filtered[0].flag
+            }
+            return (
+                <div>
+                    <h1>{theCountry.name}</h1>
+                    capital {theCountry.capital}
+                    population {theCountry.population}
+                    <h2>languages</h2>
+                    <ul>
+                        {theCountry.languages.map(
+                            language =>
+                                <li key={language.name}>
+                                    {language.name}
+                                </li>
+                        )}
+                    </ul>
+                    <img width={150} height={150} src={theCountry.flag}/>
+                </div>
+            )
+        }
+        if (filtered.length <10) {
+            return (
+                <div>
+                    <ul>
+                        {countries.map(
+                            country =>
+                                <Country key={country.name} country={country}/>
+                        )}
+                    </ul>
+                </div>
+            )
+        }
+        return (
+            <div>
+               Too many matches, specify another filter
+            </div>
+        )
+    }
+    return (
+        <div>
+            <div>
+                <form>
+                    find countries <input
+                    value={newFilter}
+                    onChange={handleFilterChange}
+                />
+                </form>
+            </div>
+            <div>
+                <ListCountries countries={filtered}/>
+            </div>
+        </div>
+    );
+}
+
+export default App;
